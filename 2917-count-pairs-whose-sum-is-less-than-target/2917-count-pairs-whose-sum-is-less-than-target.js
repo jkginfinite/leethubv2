d254e9e4e04be9fa48853fe4c6626b10f3f57254
Joe Ganser <jkgprofessional@gmail.com>
2025-05-20 21:59:39 -0400

    Time: 0 ms (100%), Space: 55.3 MB (80.74%) - LeetHub

--- 2917-count-pairs-whose-sum-is-less-than-target/2917-count-pairs-whose-sum-is-less-than-target.js
+++ 2917-count-pairs-whose-sum-is-less-than-target/2917-count-pairs-whose-sum-is-less-than-target.js
@@ -7,8 +7,6 @@ var countPairs = function(nums, target) {
     let count = 0;
     for (let i=0; i<nums.length; i++) {
         for (let j=i+1; j<nums.length; j++) {
-            let a = nums[i];
-            let b = nums[j];
             if ((nums[i]+nums[j])<target) {
                 count+=1
             }
