e86b1d5cac02dbc3b97990570e3d70fd922fdd84
Joe Ganser <jkgprofessional@gmail.com>
2025-06-03 22:40:56 -0400

    Time: 564 ms (21.69%), Space: 17.6 MB (86.69%) - LeetHub

--- 2442-number-of-arithmetic-triplets/2442-number-of-arithmetic-triplets.py
+++ 2442-number-of-arithmetic-triplets/2442-number-of-arithmetic-triplets.py
@@ -2,8 +2,8 @@ class Solution:
     def arithmeticTriplets(self, nums: List[int], diff: int) -> int:
         count=0
         for i in range(len(nums)):
-            for j in range(i,len(nums)):
-                for k in range(j,len(nums)):
-                    if (nums[j]-nums[i])==(nums[k]-nums[j]) and (nums[j]-nums[i]==diff) and k>j and j>i:
+            for j in range(i+1,len(nums)):
+                for k in range(j+1,len(nums)):
+                    if (nums[j]-nums[i])==diff and (nums[k]-nums[j])==diff:# and k>j and j>i:
                         count+=1
         return count
