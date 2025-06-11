e08481dfa4ab50bbb6a0958eadf49eeac0bd4143
Joe Ganser <jkgprofessional@gmail.com>
2025-06-11 19:48:35 -0400

    Time: 0 ms (100%), Space: 17.6 MB (75.84%) - LeetHub

--- 1454-remove-palindromic-subsequences/1454-remove-palindromic-subsequences.py
+++ 1454-remove-palindromic-subsequences/1454-remove-palindromic-subsequences.py
@@ -1,6 +1,14 @@
 class Solution:
-    def isPalindrome(self, x):
-        return x==x[::-1]
+    def isPalindrome(self, s):
+        left = 0
+        right = len(s)-1
+        while left<right:
+            if s[left]!=s[right]:
+                return False
+            left+=1
+            right-=1
+        return True
+
     def removePalindromeSub(self, s: str) -> int:
         if not s:
             return 0
