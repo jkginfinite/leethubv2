2847e84f9cde821e44d809132a671bbfc89add30
Joe Ganser <jkgprofessional@gmail.com>
2025-06-14 12:55:41 -0400

    Time: 327 ms (23.82%), Space: 25.7 MB (61.12%) - LeetHub

--- 1498-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1498-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.py
+++ 1498-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1498-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.py
@@ -8,19 +8,24 @@
 class Solution:
     def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
         #the strategy is to use BFS to traverse both trees simultaneously. If we come upon the target in the original's node, we return the node of the copy
-        queue_o = deque([original])
-        queue_c = deque([cloned])
+        stack_o = []
+        stack_c = []
+        node_o = original
+        node_c = cloned
 
-        while queue_o:
-            node_o = queue_o.popleft()
-            node_c = queue_c.popleft()
+        while stack_o or node_c:
+            while node_o:
+                stack_o.append(node_o)
+                stack_c.append(node_c)
+
+                node_o = node_o.left
+                node_c = node_c.left
+            
+            node_o = stack_o.pop()
+            node_c = stack_c.pop()
 
             if node_o is target:
                 return node_c
             
-            if node_o:
-                queue_o.append(node_o.left)
-                queue_o.append(node_o.right)
-
-                queue_c.append(node_c.left)
-                queue_c.append(node_c.right)
+            node_o = node_o.right
+            node_c = node_c.right
