47a8413aae7616f0a1bfb5366a0474518db20b63
Joe Ganser <jkgprofessional@gmail.com>
2025-07-02 13:23:40 -0400

    Time: 22 ms (5.03%), Space: 17.9 MB (39.74%) - LeetHub

--- 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.py
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.py
@@ -2,7 +2,9 @@ class Solution:
     def kthCharacter(self, k: int) -> str:
         alphabet = list('abcdefghijklmnopqrstuvwxyz')
         word = alphabet[0]
-        while len(word)<k:
+        def recursive(word,k):
+            if len(word)>=k:
+                return word[k-1]
             new = ''
             for i in word:
                 index = alphabet.index(i)
@@ -12,4 +14,5 @@ class Solution:
                     next_char = alphabet[(26%index)+1]
                 new+=next_char
             word+=new
-        return word[k-1]
+            return recursive(word,k)
+        return recursive(word,k)
