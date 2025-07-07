6a61c4a21b41c4f4c7e763c8d00ef4e50e73e764
Joe Ganser <jkgprofessional@gmail.com>
2025-07-07 17:36:56 -0400

    Time: 776 ms (17.71%), Space: 0B (100%) - LeetHub

---
+++ 2829-find-latest-salaries/2829-find-latest-salaries.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select emp_id, firstname, lastname, max(salary) as salary, department_id
+from salary
+group by emp_id, firstname, lastname, department_id
+order by emp_id ASC
