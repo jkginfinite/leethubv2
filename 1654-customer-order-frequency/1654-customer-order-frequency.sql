f46839fc4ad2792681b0aef4c4dd575ae98693d4
Joe Ganser <jkgprofessional@gmail.com>
2025-07-08 11:02:40 -0400

    Time: 1846 ms (5.04%), Space: 0B (100%) - LeetHub

--- 1654-customer-order-frequency/1654-customer-order-frequency.sql
+++ 1654-customer-order-frequency/1654-customer-order-frequency.sql
@@ -1,31 +1,9 @@
-# Write your MySQL query statement below
-with t1 as
-(select customer_id, name
+select customer_id, name
 from customers
 join orders
 using (customer_id)
 join product
 using (product_id)
-where order_date like '2020-06%'
-group by 1,2
-having sum(price*quantity)>=100),
-t2 as
-(select customer_id, name
-from customers
-join orders
-using (customer_id)
-join product
-using (product_id)
-where order_date like '2020-07%'
-group by 1,2
-having sum(price*quantity)>=100)
-
-select distinct *
-from
-(select *
-from t1
-where customer_id in (select customer_id from t2)
-union all
-select *
-from t2
-where customer_id in (select customer_id from t1)) X
+where YEAR(order_date)=2020
+group by customer_id
+HAVING sum(IF(MONTH(order_date)=6,quantity,0)*price)>=100 AND SUM(IF(MONTH(order_date)=7,quantity,0)*price)>=100
