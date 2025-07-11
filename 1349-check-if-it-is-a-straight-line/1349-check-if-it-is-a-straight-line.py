24796d3799c083e4870541b46fa9c593223212f7
Joe Ganser <jkgprofessional@gmail.com>
2025-07-11 09:36:06 -0400

    Time: 0 ms (100%), Space: 29.3 MB (11.56%) - LeetHub

--- 1349-check-if-it-is-a-straight-line/1349-check-if-it-is-a-straight-line.py
+++ 1349-check-if-it-is-a-straight-line/1349-check-if-it-is-a-straight-line.py
@@ -5,13 +5,13 @@ class Solution:
         if coordinates[1][0]-coordinates[0][0]!=0:
             m = (coordinates[1][1]-coordinates[0][1])/(coordinates[1][0]-coordinates[0][0])
         elif coordinates[1][0]-coordinates[0][0]==0:
-            m = 1e27#numpy.inf
+            m = numpy.inf
         print(m)
         for index in range(2,len(coordinates)):
             if (coordinates[index][0]-coordinates[index-1][0])!=0:
                 _ = (coordinates[index][1]-coordinates[index-1][1])/(coordinates[index][0]-coordinates[index-1][0])
             elif coordinates[index][0]-coordinates[index-1][0]==0:
-                _ = 1e27 #numpy.inf
+                _ = numpy.inf
             if _!=m:
                 return False
         return True
