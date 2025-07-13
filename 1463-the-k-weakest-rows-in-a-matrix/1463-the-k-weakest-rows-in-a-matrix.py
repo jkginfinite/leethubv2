41f7e6057d02452438ab3bfbd17a65efeb1d9e2b
Joe Ganser <jkgprofessional@gmail.com>
2025-07-13 15:53:40 -0400

    Time: 0 ms (100%), Space: 18.2 MB (40.56%) - LeetHub

--- 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.py
+++ 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.py
@@ -1,7 +1,29 @@
+import heapq
 class Solution:
     def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
-        d = {}
-        for index,row in enumerate(mat):
-            d[index] = sum(row)
-        d = sorted(d.items(),key=lambda x: x[1])
-        return [v[0] for v in d][:k]
+        #use a min heap to store tuples of the sum of soldiers and their respect row indices
+        min_heap = []
+
+        #caclualte the sum of soldiers for each row and store in the min heap
+        for row in range(len(mat)):
+            soldier_count = self.binary_search(mat[row])
+            heapq.heappush(min_heap,(soldier_count, row))
+
+        #extract the k weakest row indices
+        weakest_rows = []
+        for i in range(k):
+            weakest_rows.append(heapq.heappop(min_heap)[1])
+
+        return weakest_rows
+
+    def binary_search(self, row):
+        left, right = 0, len(row)-1
+
+        #perform binary search to the find the count of soldiers
+        while left<=right:
+            mid = (left+right)//2
+            if row[mid]==1:
+                left = mid+1
+            else:
+                right = mid-1
+        return left
