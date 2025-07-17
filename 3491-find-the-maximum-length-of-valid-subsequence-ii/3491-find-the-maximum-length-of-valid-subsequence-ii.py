3b6264c05ff3904cc952b56210dbabc2d7b1c669
Joe Ganser <jkgprofessional@gmail.com>
2025-07-17 13:22:21 -0400

    Time: 1377 ms (76.23%), Space: 25.8 MB (35.25%) - LeetHub

--- 3491-find-the-maximum-length-of-valid-subsequence-ii/3491-find-the-maximum-length-of-valid-subsequence-ii.py
+++ 3491-find-the-maximum-length-of-valid-subsequence-ii/3491-find-the-maximum-length-of-valid-subsequence-ii.py
@@ -2,9 +2,9 @@ class Solution:
     def maximumLength(self, nums: List[int], k: int) -> int:
         dp=[[0]*k for _ in range(k)]
         res = 0
-        for num in nums:
-            num%=k
+        for cur in nums:
+            cur%=k
             for prev in range(k):
-                dp[prev][num] = dp[num][prev]+1
-                res = max(res, dp[prev][num])
+                dp[prev][cur] = dp[cur][prev]+1
+                res = max(res, dp[prev][cur])
         return res
