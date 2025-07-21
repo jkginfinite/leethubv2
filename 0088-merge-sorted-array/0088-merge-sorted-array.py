d212a289d49eac7fa2c4b76a28b88ad2fc6bb13f
Joe Ganser <jkgprofessional@gmail.com>
2025-07-21 14:41:53 -0400

    Time: 0 ms (100%), Space: 17.8 MB (43.28%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,5 +3,18 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        nums1[m:]=nums2
-        nums1.sort()
+        #make a copy of the first m elements of nums1
+        nums1_copy = nums1[:m]
+
+        #read pointers for nums1copy and nums2 respectively
+        p1=0
+        p2=0
+
+        #compare elements from num1copy and nums2 and write smallest to nums1
+        for p in range(n+m):
+            if p2>=n or (p1<m and nums1_copy[p1]<=nums2[p2]):
+                nums1[p]=nums1_copy[p1]
+                p1+=1
+            else:
+                nums1[p]=nums2[p2]
+                p2+=1
