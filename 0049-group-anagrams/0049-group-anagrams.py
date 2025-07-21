9c07ab9d596b1e8ae53803feaeb83d4072d85c9f
Joe Ganser <jkgprofessional@gmail.com>
2025-07-21 11:08:54 -0400

    Time: 11 ms (82.79%), Space: 22.5 MB (32.64%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,30 +1,33 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        #loop through the list
-        #sort each string, saving the index of each
-        #for the sorted list, identify indicies that are equal when sorted
-        #loop through these indices, append the result to a list
+        """
+        Steps:
+        loop through each string, sorting the characters and converting the sorted list to a string, saving that string to a new list
+save these sorted strings to a set to remove duplicates
+create a dictionary where the keys are the set of sorted strings and the values are empty lists
+loop through the set in step 1, adding all the indices where they key matched
+create an empty list
+loop through the dictionary in step 4, using the indices to identify the words and appending them to a list
+Return the list of step5
+        """
         sorts = []
         sortsSet = set()
         for s in strs:
-            S = ''.join(sorted(s))
-            sorts.append(S)
-            sortsSet.add(S)
+            S = ''.join(sorted(s)) #step 1
+            sorts.append(S) #step 1
+            sortsSet.add(S) #step 2
         
-        L =[]
-        for e in sortsSet:
-            L.append(e)
         
-        d = {key:[] for key in L}
+        d = {key:[] for key in sortsSet} #step3
         for index in range(len(sorts)):
             key = sorts[index]
-            d[key].append(index)
+            d[key].append(index) #step 4
         
-        elements = []
+        elements = [] #step 5
         for key in d.keys():
             u = []
             for value in d[key]:
                 u.append(strs[value])
-            elements.append(u)
-        return elements
+            elements.append(u)  #step 6
+        return elements #step 7
                 
