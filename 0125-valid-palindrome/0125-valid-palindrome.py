f052f4a9a4b2ee22e05ed29de8539b9c4e560499
Joe Ganser <jkgprofessional@gmail.com>
2025-07-21 14:47:43 -0400

    Time: 4 ms (90.59%), Space: 19.2 MB (28.31%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,9 +1,6 @@
 import re
-class Solution:
-    def validPalindrome(self, s):
-        return s==s[::-1]
-    
+class Solution:    
     def isPalindrome(self, s: str) -> bool:
         s = re.sub(r'[^a-zA-Z0-9]','',s.lower())
         s = s.replace(' ','')
-        return self.validPalindrome(s)
+        return s==s[::-1]
