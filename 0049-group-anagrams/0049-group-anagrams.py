6a1095a4d96d90cc1f34bfa4655338937fb9c001
Joe Ganser <jkgprofessional@gmail.com>
2025-07-21 13:07:06 -0400

    Time: 19 ms (36.92%), Space: 23 MB (5.38%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,33 +1,30 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        """
-        Steps:
-        loop through each string, sorting the characters and converting the sorted list to a string, saving that string to a new list
-save these sorted strings to a set to remove duplicates
-create a dictionary where the keys are the set of sorted strings and the values are empty lists
-loop through the set in step 1, adding all the indices where they key matched
-create an empty list
-loop through the dictionary in step 4, using the indices to identify the words and appending them to a list
-Return the list of step5
-        """
+        #loop through the list
+        #sort each string, saving the index of each
+        #for the sorted list, identify indicies that are equal when sorted
+        #loop through these indices, append the result to a list
         sorts = []
         sortsSet = set()
         for s in strs:
-            S = ''.join(sorted(s)) #step 1
-            sorts.append(S) #step 1
-            sortsSet.add(S) #step 2
+            S = ''.join(sorted(s))
+            sorts.append(S)
+            sortsSet.add(S)
         
+        L =[]
+        for e in sortsSet:
+            L.append(e)
         
-        d = {key:[] for key in sortsSet} #step3
+        d = {key:[] for key in L}
         for index in range(len(sorts)):
             key = sorts[index]
-            d[key].append(index) #step 4
+            d[key].append(index)
         
-        elements = [] #step 5
+        elements = []
         for key in d.keys():
             u = []
             for value in d[key]:
                 u.append(strs[value])
-            elements.append(u)  #step 6
-        return elements #step 7
+            elements.append(u)
+        return elements
                 
