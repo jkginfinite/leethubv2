f3a4ca7ac6c2bcb213358c2c307f5a54450daa3f
Joe Ganser <jkgprofessional@gmail.com>
2025-07-23 15:25:10 -0400

    Time: 95 ms (92.81%), Space: 42.9 MB (81.57%) - LeetHub

---
+++ 1308-smallest-string-with-swaps/1308-smallest-string-with-swaps.py
@@ -0,0 +1,46 @@
+# UnionFind class
+class UnionFind:
+    def __init__(self, size):
+        self.root = [i for i in range(size)]
+        # Use a rank array to record the height of each vertex, i.e., the "rank" of each vertex.
+        # The initial "rank" of each vertex is 1, because each of them is
+        # a standalone vertex with no connection to other vertices.
+        self.rank = [1] * size
+
+    # The find function here is the same as that in the disjoint set with path compression.
+    def find(self, x):
+        if x == self.root[x]:
+            return x
+        # Some ranks may become obsolete so they are not updated
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
+
+    # The union function with union by rank
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX != rootY:
+            if self.rank[rootX] > self.rank[rootY]:
+                self.root[rootY] = rootX
+            elif self.rank[rootX] < self.rank[rootY]:
+                self.root[rootX] = rootY
+            else:
+                self.root[rootY] = rootX
+                self.rank[rootX] += 1
+
+    def connected(self, x, y):
+        return self.find(x) == self.find(y)
+
+class Solution:
+    def smallestStringWithSwaps(self, s: str, pairs: List[List[int]]) -> str:
+        uf, res, m = UnionFind(len(s)), [], defaultdict(list)
+
+        for x,y in pairs:
+            uf.union(x,y)
+        for i in range(len(s)):
+            m[uf.find(i)].append(s[i])
+        for comp_id in m.keys():
+            m[comp_id].sort(reverse=True)
+        for i in range(len(s)):
+            res.append(m[uf.find(i)].pop())
+        return ''.join(res)
