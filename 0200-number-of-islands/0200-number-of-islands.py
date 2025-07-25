410ad5b0378313e17c6e744e7781b75e21f4898a
Joe Ganser <jkgprofessional@gmail.com>
2025-07-25 12:02:03 -0400

    Time: 238 ms (74.71%), Space: 20.3 MB (42.82%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -0,0 +1,31 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+        
+        nr = len(grid)
+        nc = len(grid[0])
+        num_islands=0
+
+        for r in range(nr):
+            for c in range(nc):
+                if grid[r][c]=="1":
+                    num_islands+=1
+                    grid[r][c]=0
+                    neighbors=[]
+                    neighbors.append((r,c))
+                    while neighbors:
+                        row,col = neighbors.pop(0)
+                        if row-1>=0 and grid[row-1][col]=="1":
+                            neighbors.append((row-1,col))
+                            grid[row-1][col]="0"
+                        if row+1<nr and grid[row+1][col]=="1":
+                            neighbors.append((row+1,col))
+                            grid[row+1][col]="0"
+                        if col-1>=0 and grid[row][col-1]=="1":
+                            neighbors.append((row,col-1))
+                            grid[row][col-1]="0"
+                        if col+1<nc and grid[row][col+1]=="1":
+                            neighbors.append((row,col+1))
+                            grid[row][col+1]="0"
+        return num_islands
