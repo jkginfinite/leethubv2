62bbd431ad80c27aebb77d856d1d8138842f6a57
Joe Ganser <jkgprofessional@gmail.com>
2025-07-30 14:48:52 -0400

    Time: 443 ms (90.17%), Space: 20.7 MB (69.29%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -3,23 +3,25 @@ class Solution:
         res = []
         nums.sort()
         for i in range(len(nums)):
-            if nums[i] > 0:
+            if nums[i]>0:
                 break
-            if i == 0 or nums[i - 1] != nums[i]:
-                self.twoSumII(nums, i, res)
+            if i==0 or nums[i-1]!=nums[i]:
+                self.binarySearch(nums,i,res)
         return res
-
-    def twoSumII(self, nums: List[int], i: int, res: List[List[int]]):
-        lo, hi = i + 1, len(nums) - 1
-        while lo < hi:
-            sum = nums[i] + nums[lo] + nums[hi]
-            if sum < 0:
-                lo += 1
-            elif sum > 0:
-                hi -= 1
+        
+    def binarySearch(self,nums,i,res):
+        low = i+1
+        high = len(nums)-1
+        while low<high:
+            sum = nums[i]+nums[low]+nums[high]
+            if sum<0:
+                low+=1
+            elif sum>0:
+                high-=1
             else:
-                res.append([nums[i], nums[lo], nums[hi]])
-                lo += 1
-                hi -= 1
-                while lo < hi and nums[lo] == nums[lo - 1]:
-                    lo += 1
+                res.append([nums[i],nums[low],nums[high]])
+                low+=1
+                high-=1
+                while low<high and nums[low]==nums[low-1]:
+                    low+=1
+        
