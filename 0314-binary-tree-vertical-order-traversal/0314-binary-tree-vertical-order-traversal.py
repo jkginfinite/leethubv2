04d12f44dc04746ed905a09743595fdb59e071c3
Joe Ganser <jkgprofessional@gmail.com>
2025-07-31 14:12:35 -0400

    Time: 0 ms (100%), Space: 17.9 MB (22.1%) - LeetHub

--- 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.py
+++ 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.py
@@ -6,19 +6,13 @@
 #         self.right = right
 class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        #use breadth first search to find levels
-        #sort the levels accordingly
         if not root:
             return []
-        from collections import deque
-
         level = 0
         queue = [[root,level]]
-        #queue = deque([(root,level)])
-        levels = {0:[root.val]}
+        levels = {level:[root.val]}
         while queue:
-            (node,level) = queue.pop(0)#popleft()
-                
+            node,level = queue.pop(0)
             if node.left:
                 queue.append([node.left,level-1])
                 if level-1 not in levels:
@@ -29,9 +23,9 @@ class Solution:
                 if level+1 not in levels:
                     levels[level+1]=[]
                 levels[level+1].extend([node.right.val])
-
         s = sorted(levels.items())
         L = []
         for v in s:
             L.append(v[1])
         return L
+        
