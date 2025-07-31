b00253abdbac7852c2c4c2b96da034c0df948aa6
Joe Ganser <jkgprofessional@gmail.com>
2025-07-30 21:16:13 -0400

    Time: 0 ms (100%), Space: 19.9 MB (97.26%) - LeetHub

--- 1150-two-sum-bsts/1150-two-sum-bsts.py
+++ 1150-two-sum-bsts/1150-two-sum-bsts.py
@@ -7,24 +7,31 @@
 class Solution:
     def twoSumBSTs(self, root1: Optional[TreeNode], root2: Optional[TreeNode], target: int) -> bool:
 
-        def return_path(root1):
-            if not root1:
-                return []
-            path1 = []
-            stack1 = [root1]
-            while stack1:
-                node = stack1.pop()
-                path1.append(node.val)
-                if node.left:
-                    stack1.append(node.left)
-                if node.right:
-                    stack1.append(node.right)
-            return path1
+        def inorder(node):
+            stack = []
+            result = []
+            while node or stack:
+                while node:
+                    stack.append(node)
+                    node=node.left
+                if stack:
+                    node = stack.pop()
+                    result.append(node.val)
+                    node = node.right
+            return result
 
-        path1 = return_path(root1)
-        path2 = return_path(root2)
 
-        for i in path1:
-            if target - i in path2:
+        path1 = inorder(root1)
+        path2 = inorder(root2)
+
+        i=0
+        j=len(path2)-1
+        while i<len(path1) and j>=0:
+            curr_sum = path1[i]+path2[j]
+            if curr_sum==target:
                 return True
+            elif curr_sum<target:
+                i+=1
+            else:
+                j-=1
         return False
