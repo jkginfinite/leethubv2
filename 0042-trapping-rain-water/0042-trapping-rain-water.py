4414a6877b765f5dffa72bb309791fecc2d47d1f
Joe Ganser <jkgprofessional@gmail.com>
2025-07-31 12:36:13 -0400

    Time: 11 ms (70.9%), Space: 19.6 MB (11.69%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,19 +1,28 @@
 class Solution:
     def trap(self, height):
-        ans = 0
-        current = 0
-        st = []
-        while current < len(height):
-            while len(st) != 0 and height[current] > height[st[-1]]:
-                top = st[-1]
-                st.pop()
-                if len(st) == 0:
+        #use a stack to store the indices of the bars
+        #iterate the array
+        #while stakc is not empty and height[current]>height[st.top()]
+            #it means that the stack element canbe popped, pop the top element as top
+            #find the distance between the current element and the element at the top of the stack which is to be filled
+            #distance = current - st.top()-1
+            #find the bounded height
+            # bounded_height = min(height[current],height[st.top()])-height[top]
+            #addd the resulting trapped water to the answer ans+=distance*bounded_height
+        #push current index to top of the stack
+        #move current to the next position
+        ans=0
+        current=0
+        stack = []
+        while current<len(height):
+            while len(stack)!=0 and height[current]>height[stack[-1]]:
+                top=stack[-1]
+                stack.pop()
+                if len(stack)==0:
                     break
-                distance = current - st[-1] - 1
-                bounded_height = (
-                    min(height[current], height[st[-1]]) - height[top]
-                )
-                ans += distance * bounded_height
-            st.append(current)
-            current += 1
+                distance = current-stack[-1]-1
+                bounded_height = (min(height[current],height[stack[-1]])-height[top])
+                ans+=distance*bounded_height
+            stack.append(current)
+            current+=1
         return ans
