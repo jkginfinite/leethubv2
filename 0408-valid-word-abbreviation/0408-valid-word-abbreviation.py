54175162c3636d1c2bb433eebfd28af2fc643212
Joe Ganser <jkgprofessional@gmail.com>
2025-07-31 16:39:38 -0400

    Time: 0 ms (100%), Space: 17.8 MB (37.83%) - LeetHub

--- 0408-valid-word-abbreviation/0408-valid-word-abbreviation.py
+++ 0408-valid-word-abbreviation/0408-valid-word-abbreviation.py
@@ -1,19 +1,21 @@
 class Solution:
     def validWordAbbreviation(self, word: str, abbr: str) -> bool:
-        i=j=0
-        while j<len(abbr) and i<len(word):
-            if abbr[j].isalpha():
-                if abbr[j]!=word[i]:
+        i=j=0 #i for word, j for abbr
+        while i<len(word) and j<len(abbr):
+            if abbr[j].isdigit():
+                if abbr[j]=='0':
+                    return False #leading zero is invalid
+                num=0
+                while j<len(abbr) and abbr[j].isdigit():
+                    num = num*10+int(abbr[j])
+                    j+=1
+                i+=num
+            else:
+                if i>=len(word) or word[i]!=abbr[j]:
                     return False
                 i+=1
                 j+=1
-            else:
-                if abbr[j]=='0':
-                    return False
-                temp=0
-                while j<len(abbr) and abbr[j].isdigit():
-                    temp = temp*10+int(abbr[j])
-                    j+=1
-                i+=temp
+        return i==len(word) and j==len(abbr)
 
-        return j==len(abbr) and i==len(word)
+        #this is a two pointer approach where the pointers traverse the word and the abbreviation
+        #if the rules are violated, return false. if they traverse to the end, then theyre correct
