026e7ee5ba653d3b7be7554962c967dd0a6dfaee
Joe Ganser <jkgprofessional@gmail.com>
2025-07-30 23:23:36 -0400

    Time: 235 ms (57.31%), Space: 0B (100%) - LeetHub

--- 0612-shortest-distance-in-a-plane/0612-shortest-distance-in-a-plane.sql
+++ 0612-shortest-distance-in-a-plane/0612-shortest-distance-in-a-plane.sql
@@ -3,7 +3,6 @@ SELECT
 FROM
     Point2D p1
         JOIN
-    Point2D p2 ON (p1.x <= p2.x AND p1.y < p2.y)
-        OR (p1.x <= p2.x AND p1.y > p2.y)
-        OR (p1.x < p2.x AND p1.y = p2.y)
+    Point2D p2 ON 
+    (p1.x <= p2.x AND p1.y < p2.y) OR (p1.x <= p2.x AND p1.y > p2.y) OR (p1.x < p2.x AND p1.y = p2.y)
 ;
