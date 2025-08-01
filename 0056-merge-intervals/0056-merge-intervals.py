b8a30a7e9c9bffa5a055811f344aabece3b8efcc
Joe Ganser <jkgprofessional@gmail.com>
2025-07-31 21:51:25 -0400

    Time: 3 ms (93.27%), Space: 21 MB (62.49%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,33 +1,11 @@
 class Solution:
-    def bfs(self, intervals: List[List[int]]) -> List[List[int]]:
-        if len(intervals)==1:
-            return intervals
-
-        merged = []
-        queue = sorted(intervals)
-        while queue:
-            node1 = queue.pop(0)
-            if queue:
-                node2 = queue[0]
-                if node1[-1]>=node2[0]:
-                    interval = [min(node1+node2),max(node1+node2)]
-                    merged.append(interval)
-                    queue.pop(0)
-                else:
-                    merged.append(node1)
-            else:
-                merged.append(node1)
-        return merged
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-
-        def recursion(intervals):
-            queue = self.bfs(intervals)
-            if len(queue)==1:
-                return queue
-            for index in range(1,len(queue)):
-                elemen_next = queue[index]
-                elemen = queue[index-1]
-                if elemen[-1]>=elemen_next[0]:
-                    return recursion(queue)
-            return queue
-        return recursion(intervals)
+        intervals.sort(key=lambda x: x[0])
+        merged = []
+        for interval in intervals:
+            #if the list of merged intervals is empty or if the current interval does not overlap with the previous, append it
+            if not merged or merged[-1][1]<interval[0]:
+                merged.append(interval)
+            else:
+                merged[-1][1] = max(merged[-1][1],interval[1])
+        return merged
