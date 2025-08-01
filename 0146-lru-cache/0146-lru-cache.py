584632aa3079ce055e12118a3c113dc25832aeab
Joe Ganser <jkgprofessional@gmail.com>
2025-07-31 20:36:07 -0400

    Time: 120 ms (78.06%), Space: 78 MB (68.15%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,19 +1,53 @@
-import collections
+class ListNode:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.next = None
+        self.prev = None
+
 class LRUCache:
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.dic = collections.OrderedDict()
+        self.dic = {}
+        self.head = ListNode(-1,-1)
+        self.tail = ListNode(-1,-1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
 
-    def get(self, key):
+    def get(self, key: int) -> int:
         if key not in self.dic:
             return -1
-        self.dic.move_to_end(key)
-        return self.dic[key]
-    
-    def put(self, key, value):
-        if key in self.dic:
-            self.dic.move_to_end(key)
+        node = self.dic[key]
+        self.remove(node)
+        self.add(node)
+        return node.val
 
-        self.dic[key] = value
+    def put(self, key: int, value: int) -> None:
+        if key in self.dic:
+            old_node = self.dic[key]
+            self.remove(old_node)
+        node = ListNode(key,value)
+        self.dic[key]=node
+        self.add(node)
         if len(self.dic)>self.capacity:
-            self.dic.popitem(False)
+            node_to_delete = self.head.next
+            self.remove(node_to_delete)
+            del self.dic[node_to_delete.key]
+
+    def add(self,node):
+        previous_end = self.tail.prev
+        previous_end.next = node
+        node.prev = previous_end
+        node.next = self.tail
+        self.tail.prev = node
+
+    def remove(self,node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+        
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
