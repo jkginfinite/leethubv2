fe992889ba074623858ffec1102785f8fc75ac40
Joe Ganser <jkgprofessional@gmail.com>
2025-08-03 21:19:57 -0400

    Time: 245 ms (60.23%), Space: 20.1 MB (82.35%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,31 +1,32 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
+        m = len(grid)-1
+        n = len(grid[0])-1
+        def getneighbors(row,col):
+            neighbors = []
+            if row+1<=m:
+                neighbors.append([row+1,col])
+            if row-1>=0:
+                neighbors.append([row-1,col])
+            if col+1<=n:
+                neighbors.append([row,col+1])
+            if col-1>=0:
+                neighbors.append([row,col-1])
+            return neighbors
         
-        nr = len(grid)
-        nc = len(grid[0])
-        num_islands=0
+        islands = 0
+        for row_index in range(m+1):
+            for col_index in range(n+1):
+                if grid[row_index][col_index]=="1":
+                    #find all cells associated with this island
+                    stack = [[row_index,col_index]]
+                    islands+=1
+                    while stack:
+                        r,c = stack.pop()
+                        grid[r][c]="0"
+                        for neighbor in getneighbors(r,c):    
+                            rr,cc = neighbor
+                            if grid[rr][cc]=='1':
+                                stack.append(neighbor)
 
-        for r in range(nr):
-            for c in range(nc):
-                if grid[r][c]=="1":
-                    num_islands+=1
-                    grid[r][c]=0
-                    neighbors=[]
-                    neighbors.append((r,c))
-                    while neighbors:
-                        row,col = neighbors.pop(0)
-                        if row-1>=0 and grid[row-1][col]=="1":
-                            neighbors.append((row-1,col))
-                            grid[row-1][col]="0"
-                        if row+1<nr and grid[row+1][col]=="1":
-                            neighbors.append((row+1,col))
-                            grid[row+1][col]="0"
-                        if col-1>=0 and grid[row][col-1]=="1":
-                            neighbors.append((row,col-1))
-                            grid[row][col-1]="0"
-                        if col+1<nc and grid[row][col+1]=="1":
-                            neighbors.append((row,col+1))
-                            grid[row][col+1]="0"
-        return num_islands
+        return islands
