babcb86e0d05f2659db2f680b942d62227dc107f
Joe Ganser <jkgprofessional@gmail.com>
2025-08-06 21:09:00 -0400

    Time: 49 ms (82.72%), Space: 21.3 MB (94.22%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -7,35 +7,26 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        #start from the root node and traverse the tree
-        #until we find p and q, keep storing the parent pointers
-        #ocne weve found both p and q, we get all the ancestors for p using the parent dictionary and add to a set
-        #called ancestors
-        #similarly, we traverse through ancestors for node q. the ancestors is present in the ansestors for p
-        #this means the first ansestor common between p and q and hance LCA node
+
+        parents = {root:None}
 
         stack = [root]
-        parent = {root:None}
-
-        while p not in parent or q not in parent:
+        while p not in parents or q not in parents:
             node = stack.pop()
 
             if node.left:
-                parent[node.left] = node
+                parents[node.left] = node
                 stack.append(node.left)
-            if node.right:
-                parent[node.right]= node
-                stack.append(node.right)
-            
-        #ancestors set() for node p
-        ancestors = set()
 
-        #process all ancestors for node p using parent pointers
+            if node.right:
+                parents[node.right] = node
+                stack.append(node.right)
+        
+        ancestors = set()
         while p:
             ancestors.add(p)
-            p = parent[p]
+            p = parents[p]
 
         while q not in ancestors:
-            q = parent[q]
+            q = parents[q]
         return q
-        
