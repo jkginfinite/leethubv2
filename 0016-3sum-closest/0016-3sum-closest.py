3e0cfcf97d84171103c769224f605574754fa0a5
Joe Ganser <jkgprofessional@gmail.com>
2025-08-07 11:04:51 -0400

    Time: 314 ms (81.35%), Space: 17.7 MB (78.51%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -3,15 +3,17 @@ class Solution:
         diff = float("inf")
         nums.sort()
         for i in range(len(nums)):
-            low, high = i+1, len(nums)-1
-            while low<high:
-                sum = nums[i]+nums[low]+nums[high]
-                if abs(target-sum)<abs(diff):
-                    diff = target - sum
-                if sum<target:
-                    low+=1
+            lo,hi = i+1, len(nums)-1
+            while lo<hi:
+                Sum = nums[i]+nums[lo]+nums[hi]
+                if abs(target-Sum)<abs(diff):
+                    diff = target-Sum
+                if Sum<target:
+                    lo+=1
                 else:
-                    high-=1
+                    hi-=1
             if diff==0:
                 break
-        return target - diff
+
+        return target-diff
+        
