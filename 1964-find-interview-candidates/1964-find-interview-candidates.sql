2c0b4503d08d66d1cbb7479ceb7ec110056aeefe
Joe Ganser <jkgprofessional@gmail.com>
2025-08-07 20:22:49 -0400

    Time: 1112 ms (57.59%), Space: 0B (100%) - LeetHub

---
+++ 1964-find-interview-candidates/1964-find-interview-candidates.sql
@@ -0,0 +1,33 @@
+# Write your MySQL query statement below
+with t0 as 
+(
+    select gold_medal as user, contest_id
+    from contests
+    union all
+    select silver_medal as user, contest_id
+    from contests
+    union all
+    select bronze_medal as user, contest_id
+    from contests
+),
+t1 as (
+    select user, contest_id, row_number() over(partition by user order by contest_id) as rn
+    from t0
+),
+
+t2 as (
+    select user as user_id
+    from t1
+    group by user, contest_id - rn
+    having count(*)>=3
+    union all
+    select gold_medal as user_id
+    from contests
+    group by gold_medal
+    having count(*)>=3
+)
+
+select distinct u.name, u.mail
+from t2
+inner join users u
+on t2.user_id = u.user_id
