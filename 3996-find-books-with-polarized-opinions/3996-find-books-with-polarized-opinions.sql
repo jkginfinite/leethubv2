c4800288299b6576a20633f02b7e2965d11d9803
Joe Ganser <jkgprofessional@gmail.com>
2025-08-09 15:35:08 -0400

    Time: 543 ms (100%), Space: 0B (100%) - LeetHub

---
+++ 3996-find-books-with-polarized-opinions/3996-find-books-with-polarized-opinions.sql
@@ -0,0 +1,23 @@
+# Write your MySQL query statement below
+with book_stats as (
+    select book_id,
+    count(*) total_session,
+    min(session_rating) as minrating,
+    max(session_rating) as maxrating,
+    sum(case when session_rating>3 then 1 else 0 end) highrating,
+    sum(case when session_rating<3 then 1 else 0 end) lowrating,
+    sum(case when session_rating>3 or session_rating<3 then 1 else 0 end) extremes
+    from reading_sessions
+    group by 1
+)
+
+
+select book_id, title, author, genre, pages, maxrating-minrating as rating_spread, round(extremes*1.0/total_session,2) as polarization_score
+from book_stats
+join books
+using (book_id)
+where total_session>4
+and highrating>0
+and lowrating>0
+having polarization_score>=0.6
+order by polarization_score DESC, title DESC
