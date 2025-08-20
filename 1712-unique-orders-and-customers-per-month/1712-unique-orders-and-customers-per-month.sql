bfd7de2e26ad0f9adb6ee8b527c080bdd648b106
Joe Ganser <jkgprofessional@gmail.com>
2025-08-19 21:22:28 -0400

    Time: 602 ms (61.34%), Space: 0B (100%) - LeetHub

---
+++ 1712-unique-orders-and-customers-per-month/1712-unique-orders-and-customers-per-month.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+
+select date_format(order_date, '%Y-%m') month, count(order_date) as order_count, count(distinct customer_id) as customer_count
+from orders
+where invoice>20
+group by 1
