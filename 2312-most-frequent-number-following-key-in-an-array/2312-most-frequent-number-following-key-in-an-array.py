fbb1ab7441b4c08fc3bfe6eca6fd51b45ad77f80
Joe Ganser <jkgprofessional@gmail.com>
2025-09-01 13:25:49 -0400

    Time: 0 ms (100%), Space: 17.8 MB (66.56%) - LeetHub

--- 2312-most-frequent-number-following-key-in-an-array/2312-most-frequent-number-following-key-in-an-array.py
+++ 2312-most-frequent-number-following-key-in-an-array/2312-most-frequent-number-following-key-in-an-array.py
@@ -1,14 +1,9 @@
 class Solution:
     def mostFrequent(self, nums: List[int], key: int) -> int:
-        Maxx = 0
-        D = {}
-
-        for index in range(1,len(nums)):
-            curr = nums[index]
-            prev = nums[index-1]
-            if prev == key:
-                if curr not in D:
-                    D[curr]=0
-                D[curr]+=1
-                Maxx = max(Maxx,D[curr])
-        return [key for key in D.keys() if D[key]==Maxx][0]   
+        from collections import Counter
+        counts = Counter()
+        for i in range(len(nums)-1):
+            if nums[i]==key:
+                counts[nums[i+1]]+=1
+        res = max(counts,key=counts.get)
+        return res   
