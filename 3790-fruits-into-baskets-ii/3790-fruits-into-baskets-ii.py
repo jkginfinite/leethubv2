877c901cfe0cf46e84bd18ba7ef7e99dd2642bab
Joe Ganser <jkgprofessional@gmail.com>
2025-09-05 11:02:23 -0400

    Time: 24 ms (28.93%), Space: 18.1 MB (13.85%) - LeetHub

--- 3790-fruits-into-baskets-ii/3790-fruits-into-baskets-ii.py
+++ 3790-fruits-into-baskets-ii/3790-fruits-into-baskets-ii.py
@@ -1,20 +1,15 @@
 class Solution:
     def numOfUnplacedFruits(self, fruits: List[int], baskets: List[int]) -> int:
         queue = fruits
-        taken = []
         while queue:
             f = queue.pop(0)
             index=0
             while index<len(baskets):
                 b = baskets[index]
-                print(f"f:{f}, b:{b}")
-                if f<=b and index not in taken:
-                    #taken.append(index)
+                if f<=b:
                     baskets.pop(index)
                     index=len(baskets)
                 else:
                     index+=1
-                print(taken)
-                print('\n')
-        return len(baskets)-len(taken)
+        return len(baskets)
         
