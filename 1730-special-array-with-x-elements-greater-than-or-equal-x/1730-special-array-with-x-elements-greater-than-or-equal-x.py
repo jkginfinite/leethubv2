78a4520cd3a78fed8364f6d4173d5cfe60e01fbf
Joe Ganser <jkgprofessional@gmail.com>
2025-09-06 14:00:30 -0400

    Time: 1 ms (56.86%), Space: 17.6 MB (82.5%) - LeetHub

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/1730-special-array-with-x-elements-greater-than-or-equal-x.py
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/1730-special-array-with-x-elements-greater-than-or-equal-x.py
@@ -1,14 +1,25 @@
 class Solution:
+    def get_first_greater_or_equal(self, nums: List[int],val) -> int:
+        start = 0
+        end = len(nums)-1
+
+        index = len(nums)
+        while start<=end:
+            mid = (start+end)//2
+            if nums[mid]>=val:
+                index = mid
+                end = mid-1
+            else:
+                start = mid+1
+        return index
+
     def specialArray(self, nums: List[int]) -> int:
+        nums.sort()
         N = len(nums)
 
-        freq = [0]*(N+1)
-        for num in nums:
-            freq[min(N,num)]+=1
+        for i in range(1,N+1):
+            k = self.get_first_greater_or_equal(nums,i)
 
-        num_greater_than_or_equal = 0
-        for i in range(N,0,-1):
-            num_greater_than_or_equal+=freq[i]
-            if i==num_greater_than_or_equal:
+            if N-k==i:
                 return i
         return -1
