43521e60553846b3c8619489e687138f5f0d40eb
Joe Ganser <jkgprofessional@gmail.com>
2025-09-09 20:20:56 -0400

    Time: 0 ms (100%), Space: 17.7 MB (48.04%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.py
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.py
@@ -10,10 +10,7 @@ class Solution:
         ct = list(columnTitle)
         N_ct = len(columnTitle)-1
         for index in range(len(ct)-1,-1,-1):
-
             element = ct[index]
             value = d[element]*(26**(N_ct-index))
             position+=value
-            print(value)
-            print(position)
         return position
