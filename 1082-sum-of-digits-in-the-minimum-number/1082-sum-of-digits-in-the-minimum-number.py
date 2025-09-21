ba23d7f52e4f6822cc708dd65a0a4413c005aeed
Joe Ganser <jkgprofessional@gmail.com>
2025-09-21 19:28:29 -0400

    Time: 0 ms (100%), Space: 17.8 MB (41.54%) - LeetHub

--- 1082-sum-of-digits-in-the-minimum-number/1082-sum-of-digits-in-the-minimum-number.py
+++ 1082-sum-of-digits-in-the-minimum-number/1082-sum-of-digits-in-the-minimum-number.py
@@ -2,17 +2,6 @@ class Solution:
     def sumOfDigits(self, nums: List[int]) -> int:
 
         minn = min(nums)
-
-        """
-        def recursion(number):
-            if len(str(number))==1:
-                return number
-            numz = 0
-            for i in str(number):
-                numz+=int(i)
-            return recursion(numz)
-        """
-
         if len(str(minn))==1:
             if minn%2!=0:
                 return 0
