5c85c933889d9fa63e031c6f0faff04355f26053
Joe Ganser <jkgprofessional@gmail.com>
2025-11-02 15:22:58 -0500

    Time: 3480 ms (5.01%), Space: 19.2 MB (88.13%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.py
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -1,9 +1,9 @@
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
-        #return [next((x for x in nums[i+1:] + nums[:i] if x > nums[i]),-1) for i in range(len(nums))]
+        return [next((x for x in nums[i+1:] + nums[:i] if x > nums[i]),-1) for i in range(len(nums))]
 
-        results = []
-        for i in range(len(nums)):
-            _ = next((x for x in nums[i+1:]+nums[:i] if x>nums[i]),-1)
-            results.append(_)
-        return results
+        #results = []
+        #for i in range(len(nums)):
+        #    _ = next((x for x in nums[i+1:]+nums[:i] if x>nums[i]),-1)
+        #    results.append(_)
+        #return results
