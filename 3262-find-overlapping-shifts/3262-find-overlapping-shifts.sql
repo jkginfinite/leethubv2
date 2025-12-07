3fa0e05c9703f962999f95d4bf5d15343c92f330
Joe Ganser <jkgprofessional@gmail.com>
2025-12-07 09:37:00 -0500

    Time: 2021 ms (5.6%), Space: 0B (100%) - LeetHub

--- 3262-find-overlapping-shifts/3262-find-overlapping-shifts.sql
+++ 3262-find-overlapping-shifts/3262-find-overlapping-shifts.sql
@@ -1,6 +1,6 @@
 select a.employee_id, count(*) as overlapping_shifts
 from employeeshifts A
-join employeeshifts B
+inner join employeeshifts B
 on a.employee_id = b.employee_id and a.start_time<b.start_time and a.end_time>b.start_time
 group by 1
 order by 1 ASC
